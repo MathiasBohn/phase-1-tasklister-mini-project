c5327c5072caeff04d3ffc6299d29c6995d0262a
Mathias <mathiasbohndk@gmail.com>
2025-08-20 22:32:52 +0200

    Tests passed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("#create-task-form");
+  const list = document.querySelector("#tasks");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const task = event.target.elements["new-task-description"].value; 
+    buildToDo(task);
+    event.target.reset();
+  });
+
+  function buildToDo(task) {
+    const li = document.createElement("li");
+    li.textContent = task;
+    list.appendChild(li);
+  }
 });
